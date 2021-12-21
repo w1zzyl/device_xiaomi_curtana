eebf48726d7b621127f1bde397664ee97326ef5f
Stealth1226 <rishawniyer06@gmail.com>
2021-12-21 14:20:06 +0530

    sm6250-common: light it up!

--- common.mk
+++ common.mk
@@ -16,9 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# MiuiCamera
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
-
 # Enforce native interfaces of product partition as VNDK
 PRODUCT_PRODUCT_VNDK_VERSION := current
 
@@ -317,7 +314,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-lighthouse
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
